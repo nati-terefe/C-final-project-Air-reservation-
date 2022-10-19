36cf70fa2b126768219b0e8cfdf90a18cb454a17
temeboss <107496448+temeboss@users.noreply.github.com>
2022-10-19 18:03:58 +0300

    added error provider for register

--- Airline-reservation/Airline-reservation/register.cs
+++ Airline-reservation/Airline-reservation/register.cs
@@ -26,47 +26,47 @@ namespace Airline_reservation
         private void register_Load(object sender, EventArgs e)
         {
             // making the labels and the logo transparent 
-            usernamelabel.Parent = pictureBox1;
+            usernamelabel.Parent = phoneerror;
             usernamelabel.BackColor = Color.Transparent;
-            passwordlabel.Parent = pictureBox1;
+            passwordlabel.Parent = phoneerror;
             passwordlabel.BackColor = Color.Transparent;
-            firstnamelabel.Parent = pictureBox1;
+            firstnamelabel.Parent = phoneerror;
             firstnamelabel.BackColor=Color.Transparent;
-            lastnamelabel.Parent= pictureBox1;
+            lastnamelabel.Parent= phoneerror;
             lastnamelabel.BackColor = Color.Transparent;
-            emaillabel.Parent = pictureBox1;
+            emaillabel.Parent = phoneerror;
             emaillabel.BackColor = Color.Transparent;
-            phonelabel.Parent= pictureBox1;
+            phonelabel.Parent= phoneerror;
             phonelabel.BackColor = Color.Transparent;
-            birthdatelabel.Parent = pictureBox1;
+            birthdatelabel.Parent = phoneerror;
             birthdatelabel.BackColor = Color.Transparent;
-            genderlabel.Parent = pictureBox1;
+            genderlabel.Parent = phoneerror;
             genderlabel.BackColor = Color.Transparent;
-            registerlabel.Parent = pictureBox1;
+            registerlabel.Parent = phoneerror;
             registerlabel.BackColor = Color.Transparent;
-            profilepicturelabel.Parent = pictureBox1;
+            profilepicturelabel.Parent = phoneerror;
             profilepicturelabel.BackColor = Color.Transparent;
-            logo.Parent = pictureBox1;
+            logo.Parent = phoneerror;
             logo.BackColor = Color.Transparent;
-            hintlable.Parent = pictureBox1;
+            hintlable.Parent = phoneerror;
             hintlable.BackColor = Color.Transparent;
 
             /////making the buttons transparent
 
             ///////////////////login header button ///////////////////
-            loginheaderbutton.Parent = pictureBox1;
+            loginheaderbutton.Parent = phoneerror;
             loginheaderbutton.BackColor = Color.Transparent;
             loginheaderbutton.FlatAppearance.BorderSize = 0; // removing the boarder of the button
             ////////////// register header button //////////////////
-            registerheaderbutton.Parent = pictureBox1;
+            registerheaderbutton.Parent = phoneerror;
             registerheaderbutton.BackColor = Color.Transparent;
             registerheaderbutton.FlatAppearance.BorderSize = 0;
             ////////////// About us header button //////////////////
-            aboutheaderbutton.Parent = pictureBox1;
+            aboutheaderbutton.Parent = phoneerror;
             aboutheaderbutton.BackColor = Color.Transparent;
             aboutheaderbutton.FlatAppearance.BorderSize = 0;
             ////////////// contact us header button //////////////////
-            contactheaderbutton.Parent = pictureBox1;
+            contactheaderbutton.Parent = phoneerror;
             contactheaderbutton.BackColor = Color.Transparent;
             contactheaderbutton.FlatAppearance.BorderSize = 0;
 
@@ -96,6 +96,7 @@ namespace Airline_reservation
         private void registerbutton_Click(object sender, EventArgs e)
         {
             string firstname, lastname, email, hint, phone, username, password, bd;
+            string gender;
             firstname = firstnametextbox.Text;
             lastname = lastnametextbox.Text;
             email=emailtextbox.Text;
@@ -104,8 +105,48 @@ namespace Airline_reservation
             username = usernametextbox.Text;
             password = passwordtextbox.Text;
             bd = birthdate.ToString();
+            bool b1 = Male.Checked;
+            gender= b1 ? "male" : "female"; // ternary operator
             registerbutton.BackColor = Color.Silver;
             donebutton.BackColor = Color.Red;
+            if (string.IsNullOrEmpty(firstnametextbox.Text))
+            {
+                firstnameerror.SetError(firstnametextbox, "please enter First name");
+            }
+            if (string.IsNullOrEmpty(lastnametextbox.Text))
+            {
+                lastnameerror.SetError(lastnametextbox, "please enter Last name");
+            }
+            if (string.IsNullOrEmpty(emailtextbox.Text))
+            {
+                emailerror.SetError(emailtextbox, "please enter Email name");
+            }
+            if (string.IsNullOrEmpty(phonetextbox.Text))
+            {
+                phonerror.SetError(phonetextbox,"please enter phone number");
+            }
+            
+            if (string.IsNullOrEmpty(gendergroupbox.Text))
+            {
+                gendererror.SetError(gendergroupbox, "please enter gender");
+            }
+            if (string.IsNullOrEmpty(usernametextbox.Text))
+            {
+                usernameerror.SetError(usernametextbox, "please enter user name");
+            }
+            if (string.IsNullOrEmpty(passwordtextbox.Text))
+            {
+                passworderror.SetError(passwordtextbox, "please enter password ");
+            }
+            if (string.IsNullOrEmpty(hinttextbox.Text))
+            {
+                hinterror.SetError(hinttextbox, "please enter hint");
+            }
+
+
+
+
+
         }
 
         private void loginheaderbutton_Click(object sender, EventArgs e)
