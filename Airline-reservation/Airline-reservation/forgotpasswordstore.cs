2f796ea864e5665aecdcb59fd039179c96cd060d
temeboss <107496448+temeboss@users.noreply.github.com>
2022-10-21 19:59:58 +0300

    added property to forget password

---
+++ Airline-reservation/Airline-reservation/forgotpasswordstore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline_reservation
+{
+    internal class forgotpasswordstore
+    {
+        private static List<forgotpasswordstore> fpws = new List<forgotpasswordstore>();
+
+        public string fpsusername { get; set; }
+        public string fpspassword { get; set; }
+        
+
+        public void save()
+        {
+            fpws.Add(this);
+        }
+        public static List<forgotpasswordstore> getall()
+        {
+            return fpws;
+        }
+    }
+}
