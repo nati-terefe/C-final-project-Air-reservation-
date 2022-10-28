ec6660066b1ec7830f3994e3bb5f5566994a7c33
Samuel <sammesi22@gmail.com>
2022-10-28 20:53:59 +0300

    Structing Sql code to fit manage windows

--- SQL Query/Registered Table and related SQL.sql
+++ SQL Query/Registered Table and related SQL.sql
@@ -1,4 +1,4 @@
-use AirlineReservation -- Using Databse
+use AirlineReservation -- Using AirlineReservation Databse
 
 -- --------------------------------------------------------------------------------------------------------------------------------
 
@@ -18,9 +18,7 @@ hintA varchar(20),
 profilepic image,
 );
 
--- Initial Data to fill registerd table no pp
-Insert into registered(fname, lname, email, phoneno, birthdate, gender, usrname, passwd, hintQ, hintA)
-values('Abebe','Kebede', 'abekebe@gmail.com', '0987654321', getdate(), 'Male', 'abekebe', 'abekebe', 'Are you?', 'Yes');
+-- Initial Datas to fill registered table are done from C-Sharp by sign up
 
 select * from registered
 
@@ -30,7 +28,6 @@ drop table registered
 
 GO
 -- 1. add new registry stored procedure that insert and register new user
-
 CREATE or ALTER PROCEDURE addregistry(
 @fname varchar(20),
 @lname varchar(20),
@@ -52,11 +49,11 @@ END
 
 select * from registered
 
+-- ------------------------------------------------------------------------------------
+
 GO
-
 -- 2. full info procedure returns full information of certain username
-
-CREATE or ALTER procedure fullinfo(
+CREATE or ALTER PROCEDURE fullinfo(
 @usrname varchar(20),
 @fname varchar(20) OUTPUT,
 @lname varchar(20) OUTPUT,
@@ -74,9 +71,13 @@ Select @fname=fname,@lname=lname,@email=email,@phoneno=phoneno,@birthdate=birthd
 from registered where usrname=@usrname
 End
 
--- 3. updateregistry procedure updates registry information of certain user
+select * from registered
 
-CREATE or ALTER procedure updateregistry(
+-- ------------------------------------------------------------------------------------
+
+GO
+-- 3. updateregistry procedure updates registry information of certain user
+CREATE or ALTER PROCEDURE updateregistry(
 @initialusrname varchar(20),
 @usrname varchar(20),
 @fname varchar(20),
@@ -97,8 +98,12 @@ set fname=@fname,lname=@lname,email=@email,phoneno=@phoneno,birthdate=@birthdate
 where usrname=@initialusrname
 End
 
--- 4. deleteregistrywithlogin stored procedure that deletes an entire record
+select * from registered
 
+-- ------------------------------------------------------------------------------------
+
+GO
+-- 4. deleteregistrywithlogin stored procedure that deletes an entire record of certain username
 CREATE or ALTER PROCEDURE deleteregistrywithlogin(
 @usrname varchar(20)
 )
@@ -111,10 +116,44 @@ END
 select * from login
 select * from registered
 
+-- ------------------------------------------------------------------------------------
+
+GO
+-- 5. What is the hint question stored procedure that returns the hint question of a certain usename and password
+CREATE or ALTER PROCEDURE whathintq(
+@usrname varchar(20),
+@hintqis varchar(100) OUTPUT
+)
+AS
+BEGIN
+select @hintqis=hintQ from registered where usrname=@usrname
+END
+
+declare @hintq varchar(100)
+execute whathintq 'admin', @hintq OUTPUT;
+select @hintq
+
+-- ------------------------------------------------------------------------------------
+
+GO
+-- 6. What is the hint answer stored procedure that returns the hint answer of a certain usename
+CREATE or ALTER PROCEDURE whathinta(
+@usrname varchar(20),
+@hintais varchar(20) OUTPUT
+)
+AS
+BEGIN
+select @hintais=hintA from registered where usrname=@usrname
+END
+
+declare @hinta varchar(100)
+execute whathinta 'admin', @hinta OUTPUT;
+select @hinta
+
 -- --------------------------------------------------------------------------------------------------------------------------------
 -- Function on registered table --------------------------------------------------------------------------------------
+GO
 -- 1. What photo function returns photo of certain username
-
 CREATE or ALTER FUNCTION whatphoto(
 @usrname varchar(20)
 )
